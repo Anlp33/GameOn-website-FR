3987370423df12adb7d6b4697c40f2badb7e3418
Anne-Laure <annelaure.poire@gmail.com>
2021-10-11 10:17:01 +0200

    field function return true, validation messages to add

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,35 +7,37 @@ function editNav() {
   }
 }
 
-// DOM Elements
+/*DOM Elements*/
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
+const form = document.getElementById("form");
 const close = document.querySelector(".close");
+const btnclose = document.querySelector(".btn-close");
 const first = document.querySelector("#first");
-const last = document.querySelector(" #last");
-const email = document.querySelector("#email");
-const birthdate = document.querySelector("#birthdate");
-const quantity = document.querySelector("#quantity");
-const radio = document.querySelector(".input[type=radio]:checked");
-const checkbox = document.getElementById("checkbox1");
+const last = document.querySelector("#last");
+const modalSubmit = document.getElementById("modal-submit");
+let error = document.querySelector(".error");
 
-// launch modal event
+/*launch modal event*/
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+/*launch modal form*/
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-//close modal event
-close.addEventListener("click", closeModal);
+/*close modal event*/
+close.addEventListener("click", closeModal());
 
-//close modal form
+/*close modal form*/
 function closeModal() {
   modalbg.style.display = "none";
 }
-// Firstname validation
+
+/*close modal form after thank you message*/
+btnclose.addEventListener("click", closeModal());
+
+/*Firstname validation*/
 
 const validFirst = () => {
   let firstTrim = first.value.trim();
@@ -45,9 +47,9 @@ const validFirst = () => {
     return true;
   }
 };
-//Lastname validation
+/*Lastname validation*/
 
-const validName = () => {
+const validLast = () => {
   let lastTrim = last.value.trim();
   if (lastTrim.length < 2) {
     return false;
@@ -56,94 +58,104 @@ const validName = () => {
   }
 };
 
-//REGEX for email validation
-const email = document.querySelector("#email");
-const validEmail = (email) => {
+/*REGEX for email validation*/
+const inputEmail = document.querySelector("#email");
+
+const validEmail = (inputEmail) => {
   let emailRegExp = new RegExp(
     "^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$",
     "g"
   );
 
-  let testEmail = emailRegExp.test(email);
+  let testEmail = emailRegExp.test(inputEmail);
 
-  //REGEX test
+/*REGEX test*/
   if (testEmail) {
     return true;
   } else {
     return false;
   }
 };
-validEmail();
 
-//Quantity validation
+/*Quantity validation*/
+const quantity = document.querySelector("#quantity");
+const validQuantity = (quantity) => {
+  let quantityRegExp = new RegExp("^[0-9]$", "g");
 
-// const validQuantity = function (quantity) {
-//   let quantityRegExp = new RegExp("/^[0-9]$/", "g");
+  let testQuantity = quantityRegExp.test(quantity);
 
-//   let testQuantity = quantityRegExp.test(quantity.value);
+/*REGEX test*/
+  if (testQuantity) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-//   //REGEX test
-//   if (testQuantity) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// validQuantity();
-// //Birthdate validation
+/*Birthdate validation*/
 
-// validBirthdate = () => {
-//   if (birthdate.value >= 0) {
-//     console.log("false");
-//   } else {
-//     console.log("true");
-//   }
-// };
+let validBirthdate = () => {
+  const birthdate = document.querySelector("#birthdate").value;
+  let birthdateRegex = new RegExp(
+    "([0-9]{4}[-](0[1-9]|1[0-2])[-]([0-2]{1}[0-9]{1}|3[0-1]{1})|([0-2]{1}[0-9]{1}|3[0-1]{1})[-](0[1-9]|1[0-2])[-][0-9]{4})"
+  );
+  let testBirthdate = birthdateRegex.test(birthdate);
 
-// //Location validation
+  if (testBirthdate) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// validLocation = () => {
-//   if (radio.value.length > 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
+/*Location validation*/
 
-// //Checkbox function
+validLocation = () => {
+  const radio = document.querySelectorAll("input[type=radio]:checked").length;
+  if (radio == 1) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// validCheckbox = () => {
-//   if (checkbox.checked) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
+/*Checkbox function*/
+const checkbox = document.getElementById("checkbox1");
+validCheckbox = () => {
+  if (checkbox.checked) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-// // //Form validation
-// // const form = document.querySelector(".form");
+/* Form validation */
 
-// // form.addEventListener('submit', function (e) {
-// //    e.preventDefault();
-// //    let resultValidFirst = validFirst();
-// //    let resultValidLast = validLast();
-// //    let resultValidEmail = validEmail();
-// //    let resultValidLocation = validLocation();
-// //    let resultValidBirthdate = validBirthdate();
-// //    let resultValidQuantity = validQuantity();
-// //    let resultValidCheckbox = validCheckbox();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let resultValidFirst = validFirst();
+  let resultValidLast = validLast();
+  let resultValidEmail = validEmail(inputEmail.value);
+  let resultValidLocation = validLocation();
+  let resultValidBirthdate = validBirthdate();
+  let resultValidQuantity = validQuantity(quantity.value);
+  let resultValidCheckbox = validCheckbox();
+  let formData = document.getElementsByClassName("formData");
 
-// //   if (
-// //     resultValidFirst &&
-// //     resultValidLast &&
-// //     resultValidEmail &&
-// //     resultValidLocation &&
-// //     resultValidBirthdate &&
-// //     resultValidQuantity &&
-// //     resultValidCheckbox
-// //   ) {
-// //     form.submit();
-// //   } else {
-// //     console.log("error");
-// //   }
-// // });
+  if (
+    resultValidFirst &&
+    resultValidLast &&
+    resultValidEmail &&
+    resultValidLocation &&
+    resultValidBirthdate &&
+    resultValidQuantity &&
+    resultValidCheckbox
+  ) {
+    form.submit();
+    form.style.display = "none";
+    modalSubmit.style.display = "block";
+  } else {
+    formData.setAttribute("data-error-visible", "true");
+    // formData.appenChild(data-error);
+  }
+});
